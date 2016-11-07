073dc4de306f78b641d13239bb3183930bdae42f
wang-bin <wbsecg1@gmail.com>
2016-11-07 22:48:16 +0800

    rpi: fix linux host cross build
    toolchain: https://github.com/raspberrypi/tools/tree/master/arm-bcm2708/arm-rpi-4.9.3-linux-gnueabihf
    /opt dir from target is required in gcc sysroot
    windows toolchains have no such problems: http://sysprogs.com/files/gnutoolchains/raspberry/raspberry-gcc4.9.2-r4.exe

--- config-rpi.sh
+++ config-rpi.sh
@@ -6,3 +6,4 @@ USER_OPT="$USER_OPT --enable-omx-rpi --enable-mmal \
 TOOLCHAIN_OPT="--enable-cross-compile --cross-prefix=arm-linux-gnueabihf- --target-os=linux --arch=arm --cpu=armv6"
 EXTRA_CFLAGS="-mfloat-abi=hard -mfpu=vfp -isystem$SYSROOT/opt/vc/include -isystem$SYSROOT/opt/vc/include/IL"
 EXTRA_LDFLAGS="-L$SYSROOT/opt/vc/lib -lrt" #-lrt: clock_gettime in glibc2.17 
+test -f /bin/sh.exe || EXTRA_LDFLAGS="-Wl,-rpath-link,$SYSROOT/opt/vc/lib $EXTRA_LDFLAGS"
