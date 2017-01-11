3574ea674f7168ed437520c6d6c4bbcb44895e78
wang-bin <wbsecg1@gmail.com>
2017-01-11 10:51:06 +0800

    add a script(linux) to merge static libs to libffmpeg.a

---
+++ mergea.sh
@@ -0,0 +1,13 @@
+ADIR=${1:-$PWD}
+make_mri() {
+  echo create $ADIR/libffmpeg.a
+  for a in avutil avcodec avformat avdevice avfilter avresample swresample swscale postproc; do
+    test -f $ADIR/lib${a}.a && echo "addlib $ADIR/lib${a}.a"
+  done
+  echo save
+  echo end
+}
+
+MRI=/tmp/libffmpeg.mri
+make_mri > $MRI
+${AR:=ar} -M < $MRI
