386054388f114309d4e1b1e1e4c9b8fd4af82bcc
wang-bin <wbsecg1@gmail.com>
2017-01-21 16:05:27 +0800

    rpi: cross build with more toolchains

--- config-rpi.sh
+++ config-rpi.sh
@@ -1,17 +1,22 @@
 . config-lite.sh
+INSTALL_DIR=sdk-rpi
+: {CROSS_PREFIX:=arm-linux-gnueabihf-}
 uname -a |grep armv && {
   echo "rpi host build"
   SYSROOT=`gcc -print-sysroot`
   TOOLCHAIN_OPT="--cpu=armv6"
 } || {
   echo "rpi cross build"
-  TOOLCHAIN_OPT="--enable-cross-compile --cross-prefix=arm-linux-gnueabihf- --target-os=linux --arch=arm --cpu=armv6"
-  SYSROOT=`arm-linux-gnueabihf-gcc -print-sysroot`
+  TOOLCHAIN_OPT="--enable-cross-compile --cross-prefix=$CROSS_PREFIX --target-os=linux --arch=arm --cpu=armv6"
+  SYSROOT_CROSS=`${CROSS_PREFIX}gcc -print-sysroot`
 }
-SYSROOT=`arm-linux-gnueabihf-gcc -print-sysroot`
+: {SYSROOT:=${SYSROOT_CROSS}}
 USER_OPT="$USER_OPT --enable-omx-rpi --enable-mmal \
 --enable-muxer=mov,mp4,matroska,hls,mpegts,tee,rtsp \
 --enable-encoder=h264_omx,aac*"
+# FIXME: isystem path with spaces can not use ""
 EXTRA_CFLAGS="-mfloat-abi=hard -mfpu=vfp -isystem$SYSROOT/opt/vc/include -isystem$SYSROOT/opt/vc/include/IL"
-EXTRA_LDFLAGS="-L$SYSROOT/opt/vc/lib -lrt" #-lrt: clock_gettime in glibc2.17 
+EXTRA_LDFLAGS="-L$SYSROOT/opt/vc/lib"
+#-lrt: clock_gettime in glibc2.17
+[ "`${CROSS_PREFIX}gcc -print-file-name=librt.so`" = "librt.so" ] || EXTRA_LDFLAGS="$EXTRA_LDFLAGS -lrt"
 test -f /bin/sh.exe || EXTRA_LDFLAGS="-Wl,-rpath-link,$SYSROOT/opt/vc/lib $EXTRA_LDFLAGS"
