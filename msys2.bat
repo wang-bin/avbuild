4f963c1b78477aa492719d92ccd5765e06303f31
wang-bin <wbsecg1@gmail.com>
2017-02-12 13:57:36 +0800

    add mingw shortcuts for convenience
    must use MINGW32/64 to build ffmpeg. I find MSYSTEM in old msys2
    files(mingw32_shell.bat) does the trick

--- msys2.bat
+++ msys2.bat
@@ -1,9 +1,20 @@
 @echo off
 :: set your MSYS2_DIR here
 if [%MSYS2_DIR%] == [] set MSYS2_DIR=D:\msys2
-:: use vc to build ffmpeg. set to other value to use mingw toolchain
-set VC_BUILD=true
 
+
+:: -------------------DO NOT CHANGE THE FOLLOWING CODE------------------------
+set CC_ARG=%1%
+set OS_ARG=%2%
+set ARCH_ARG=%3%
+:: VC_BUILD is checked in build_ffmpeg.sh host build.
+set VC_BUILD=true
+if /i [%CC_ARG%] == [gcc] (
+:: MSYSTEM MUST use upper case
+  if /i [%OS_ARG%] == [mingw] set MSYSTEM=MINGW%ARCH_ARG%
+  set MINGW_BUILD=true
+  set VC_BUILD=false
+)
 
 set CPP_DIR=
 if "%ARCH%" == "arm" (
@@ -11,14 +22,17 @@ if "%ARCH%" == "arm" (
   if not exist %CPP_DIR%\cpp.exe set CPP_DIR=%MSYS2_DIR%\mingw32\bin
 )
 set PATH=%PATH%;%CPP_DIR%
-set TARGET_PARAM=vc
+set TARGET_PARAM=
+if [%VC_BUILD%] == [true] set TARGET_PARAM=vc
 if [%WINRT%] == [true] set TARGET_PARAM=winstore
 @echo Now you can run:
 @echo export FFSRC=/path/to/ffmpeg
-@echo ./build_ffmpeg.sh or ./build_ffmpeg %TARGET_PARAM%
+@echo ./build_ffmpeg.sh
+if not [%TARGET_PARAM%] == [] @echo or ./build_ffmpeg %TARGET_PARAM%
 
 if not exist %MSYS2_DIR% goto NoBash
 set HOME=%~dp0
+:: TODO: set MSYSTEM=MINGW32 for armasm to use cpp.exe?
 :: --login -x is verbose
 if [%BUILD_NOW%] == [] goto StartBash
 
