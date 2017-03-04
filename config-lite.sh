33092c4d7301f795dcb931a3be66af01f0b2e50c
wang-bin <wbsecg1@gmail.com>
2017-03-04 20:50:49 +0800

    add nellymoser audio decoder
    used by longzhu.com

--- config-lite.sh
+++ config-lite.sh
@@ -1,7 +1,7 @@
 USER_OPT="--enable-small --disable-avdevice --disable-avresample --disable-filters \
 --enable-filter=aeval,afade,aformat,all*,amix,arealtime,aresample,asplit,atempo,color*,blend,con*,draw*,eq*,fade,format,frame*,null,overlay,pad,split,volume \
 --disable-muxers --disable-encoders --disable-decoders \
---enable-decoder=aac*,ac3*,ass,cook,ssa,eac3,srt,flv,flac,h264*,hevc*,vc1*,mjpeg*,mp1*,mp2*,mp3*,mpeg*,vorbis,vp6*,vp7*,vp8*,vp9*,wma*,wmv*,opus,pcm*,wmv*,rv* \
+--enable-decoder=aac*,ac3*,ass,cook,ssa,eac3,srt,flv,flac,h264*,hevc*,vc1*,mjpeg*,mp1*,mp2*,mp3*,mpeg*,nellymoser,vorbis,vp6*,vp7*,vp8*,vp9*,wma*,wmv*,opus,pcm*,wmv*,rv* \
 --disable-demuxers --enable-demuxer=aac,concat,data,flv,hls,h264,hevc,live_flv,matroska,mjpeg*,mov,mpeg*,ac3,ass,avi,eac3,flac,flv,mp3,mxf,nsv,nut,ogg,rawvideo,rtp,rtsp,srt,vc1,v210,wav --disable-bsfs --enable-bsf=aac*,h264*,hevc*,mjpeg*,mpv*,mp3*,mpeg*,vp9* \
 --disable-parsers --enable-parser=aac*,ac3,cook,flac,h263,h264,hevc,mjpeg*,mpeg*,opus,rv*,vc1,vorbis,vp8,vp9 \
 $USER_OPT
