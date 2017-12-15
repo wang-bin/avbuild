222c9b118741dfd1d3c819dbb507b818b5fa21c3
wang-bin <wbsecg1@gmail.com>
2017-12-15 11:41:13 +0800

    add yuv4mpegpipe & wrapped_avframe. useful for testing dec fps

--- config-lite.sh
+++ config-lite.sh
@@ -4,15 +4,15 @@ USER_OPT="--enable-small --disable-avdevice --disable-avresample \
 --disable-muxers \
 --disable-encoders \
 --disable-decoders \
---enable-decoder=aac*,ac3*,alac*,ape,ass,cook,eac3,flv,flac,h264*,hevc*,mp[1-3]*,*m*peg*,nellymoser,opus,pcm*,rawvideo,rv*,srt,ssa,v210*,vc1*,vorbis,vp[6-9],wm* \
+--enable-decoder=aac*,ac3*,alac*,ape,ass,cook,eac3,flv,flac,h264*,hevc*,mp[1-3]*,*m*peg*,nellymoser,opus,pcm*,rawvideo,rv*,srt,ssa,v210*,vc1*,vorbis,vp[6-9],wm*,wrapped_avframe \
 --disable-demuxers \
---enable-demuxer=aac,ac3,ape,ass,avi,concat,dash,eac3,flac,*flv,hls,h264,hevc,matroska,mjpeg*,mlv,mov,mpeg*,mp3,mxf,nsv,nut,ogg,pcm*,rawvideo,rt*p,srt,vc1,v210*,wav \
+--enable-demuxer=aac,ac3,ape,ass,avi,concat,dash,eac3,flac,*flv,hls,h264,hevc,matroska,mjpeg*,mlv,mov,mpeg*,mp3,mxf,nsv,nut,ogg,pcm*,rawvideo,rt*p,srt,vc1,v210*,wav,yuv4mpegpipe \
 --disable-bsfs --enable-bsf=aac*,mjpeg*,*mov*,*mp*,vp9* \
 --disable-parsers --enable-parser=aac*,ac3,cook,flac,h26[3-4],hevc,mjpeg*,mpeg*,opus,rv*,vc1,vorbis,vp[8-9] \
 $USER_OPT
 "
-ENC_OPT="--enable-encoder=aac,gif,h26[3-4]*,hevc*,mjpeg,mpeg[2-4]*,nellymoser,nvenc*,opus,pcm*,rawvideo,vorbis,vp*"
-MUX_OPT="--enable-muxer=dash,fifo,gif,h264,hevc,hls,mjpeg,matroska*,mov,mp4,mpegts,nu*,og*,pcm*,rawvideo,rtp,rtsp,wav,webm"
+ENC_OPT="--enable-encoder=aac,gif,h26[3-4]*,hevc*,mjpeg,mpeg[2-4]*,nellymoser,nvenc*,opus,pcm*,rawvideo,vorbis,vp*,wrapped_avframe"
+MUX_OPT="--enable-muxer=dash,fifo,gif,h264,hevc,hls,mjpeg,matroska*,mov,mp4,mpegts,nu*,og*,pcm*,rawvideo,rtp,rtsp,wav,webm,yuv4mpegpipe"
 ENC_OPT_MOBILE="--enable-encoder=aac,gif,h264*,nellymoser,opus,pcm*"
 MUX_OPT_MOBILE="--enable-muxer=gif,hls,mov,mp4,mpegts,wav"
 # nellymoser: used by flash video, longzhu
