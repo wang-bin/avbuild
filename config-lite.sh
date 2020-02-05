624c33df4605be264689c0090850eff3cb29ef83
wang-bin <wbsecg1@gmail.com>
2020-02-05 19:22:21 +0800

    lite: more muxers, filters, encoders. #40 #34

--- config-lite.sh
+++ config-lite.sh
@@ -1,19 +1,19 @@
 USER_OPT="--enable-small --disable-avresample \
 --disable-filters \
---enable-filter=*fade,*fifo,*format,*resample,aeval,all*,atempo,color*,convolution,draw*,eq*,framerate,*_cuda,*v4l2*,hw*,null,scale,volume \
+--enable-filter=*fade,*fifo,*format,*resample,aeval,all*,atempo,color*,convolution,crop,draw*,eq*,framerate,*_cuda,*v4l2*,hw*,null,scale,volume \
 --disable-muxers \
 --disable-encoders \
 --disable-decoders \
---enable-decoder=*sub*,*text*,*web*,aac*,*ac3*,alac*,ape,ass,cc_dec,cook,dca,eac3*,truehd,flv,flac,gif,h26[3-4]*,hevc*,mp[1-3]*,*peg*,mlp,mpl2,nellymoser,opus,pcm*,*png*,rawvideo,rv*,sami,srt,ssa,v210*,vc1*,vorbis,vp[6-9]*,wm*,wrapped_avframe \
+--enable-decoder=*sub*,*text*,*web*,aac*,*ac3*,alac*,ape,ass,cc_dec,cook,dca,eac3*,truehd,ffv1,flv,flac,gif,h26[3-4]*,hevc*,mp[1-3]*,*peg*,mlp,mpl2,nellymoser,opus,pcm*,*png*,rawvideo,rv*,sami,srt,ssa,v210*,vc1*,vorbis,vp[6-9]*,wm*,wrapped_avframe \
 --disable-demuxers \
 --enable-demuxer=*sub*,*text*,*ac3,*ac,*peg*,*web*,ape,ass,avi,concat,dts*,*dash*,*flv,gif,hls,h264,hevc,kux,xv,matroska,mlv,mov,mp3,mxf,nsv,nut,ogg,pcm*,rawvideo,rt*p,spdif,srt,vc1,v210*,wav,*pipe,image2 \
 --disable-parsers --enable-parser=*sub*,aac*,ac3,cook,flac,h26[3-4],hevc,m*,opus,rv*,vc1,vorbis,vp[8-9] \
 $USER_OPT
 "
-ENC_OPT="--enable-encoder=aac,gif,h26[3-4]*,hevc*,mjpeg,*png,mpeg[2-4]*,nellymoser,nvenc*,opus,pcm*,rawvideo,vorbis,vp*,wrapped_avframe"
-MUX_OPT="--enable-muxer=dash,fifo,flv,gif,h264,hevc,hls,image2,mjpeg,matroska*,mov,mp4,mpegts,nu*,og*,pcm*,rawvideo,spdif,wav,webm,*pipe"
-ENC_OPT_MOBILE="--enable-encoder=aac,gif,h264*,nellymoser,opus,pcm*,*jpeg,*png"
-MUX_OPT_MOBILE="--enable-muxer=gif,hls,image2,mov,mp4,mpegts,wav"
+ENC_OPT="--enable-encoder=aac,gif,h26[3-4]*,hevc*,mjpeg,*png,mpeg[2-4]*,nellymoser,nvenc*,opus,pcm*,rawvideo,vorbis,vp[7-9],wrapped_avframe"
+MUX_OPT="--enable-muxer=dash,fifo,flv,gif,h264,hevc,hls,image2,*jpeg,matroska,mov,mp4,mpegts,nu*,og*,pcm*,rawvideo,spdif,wav,webm,*pipe"
+ENC_OPT_MOBILE="--enable-encoder=aac,gif,h264*,nellymoser,opus,pcm*,*jpeg,*png,vp[7-9]"
+MUX_OPT_MOBILE="--enable-muxer=*jpeg,gif,hls,image2,mov,mp4,mpegts,matroska,webm,wav"
 android_OPT="--disable-avdevice"
 ios_OPT="--disable-avdevice"
 rpi_OPT="--disable-avdevice"
