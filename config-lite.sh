b53e2cc12a5c35975a58f696563c4674621ad70d
wang-bin <wbsecg1@gmail.com>
2022-05-03 17:36:01 +0800

    lite: workaround master branch build error. add tiff
    set_color in vf_testsrc is undefined if testsrc is disabled

--- config-lite.sh
+++ config-lite.sh
@@ -1,10 +1,10 @@
 USER_OPT="--enable-small \
 --disable-filters \
---enable-filter=*null*,*fade,*fifo,*format,*resample,aeval,all*,atempo,pan,color*,convolution,crop,draw*,eq*,framerate,*_cuda,*_qsv,*_vaapi,*v4l2*,hw*,scale,volume \
+--enable-filter=*null*,*fade,*fifo,*format,*resample,aeval,all*,atempo,pan,*bars,testsrc*,color,*key,crop,draw*,eq*,framerate,*_cuda,*_qsv,*_vaapi,*v4l2*,hw*,scale,volume,test* \
 --disable-muxers \
 --disable-encoders \
 --disable-decoders \
---enable-decoder=*sub*,movtext,*web*,aac*,*ac3*,alac*,ape,ass,av1*,cc_dec,cook,dca,dnxhd,eac3*,exr,truehd,ff*,*yuv*,flv,flac,gif,h26[3-4]*,hevc*,hap,mp[1-3]*,prores,*peg*,mlp,mpl2,nellymoser,opus,pcm*,*png*,rawvideo,rv*,sami,srt,ssa,v210*,vc1*,vorbis,vp[6-9]*,wm*,wrapped_avframe \
+--enable-decoder=*sub*,movtext,*web*,aac*,*ac3*,alac*,ape,ass,av1*,cc_dec,cook,dca,dnxhd,eac3*,exr,truehd,ff*,*yuv*,flv,flac,gif,h26[3-4]*,hevc*,hap,mp[1-3]*,prores,*peg*,mlp,mpl2,nellymoser,opus,pcm*,*png*,tiff,rawvideo,rv*,sami,srt,ssa,v210*,vc1*,vorbis,vp[6-9]*,wm*,wrapped_avframe \
 --disable-demuxers \
 --enable-demuxer=*sub*,*ac3,*ac,*peg*,*web*,ape,ass,avi,concat,dnxhd,dts*,*dash*,*flv,gif,hls,h264,hevc,kux,xv,matroska,mlv,mov,mp3,mxf,nsv,nut,ogg,pcm*,rawvideo,rt*p,spdif,srt,vc1,v210*,wav,*pipe,image2 \
 --disable-parsers --enable-parser=*sub*,aac*,ac3,cook,dnxhd,flac,h26[3-4],hevc,m*,opus,rv*,vc1,vorbis,vp[8-9] \
