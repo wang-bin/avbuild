2ec763f54be0c0a3dae796ce171161c781f522f8
wang-bin <wbsecg1@gmail.com>
2022-08-27 13:45:16 +0800

    ci: no upload for pr

--- tools/ci-after-build.sh
+++ tools/ci-after-build.sh
@@ -10,5 +10,7 @@ mv sdk-* ffmpeg-${SUFFIX}
 export XZ_OPT="--threads=`getconf _NPROCESSORS_ONLN` -9e" # -9e. -8/9 will disable mt?
 tar Jcf ffmpeg-${SUFFIX}{.tar.xz,}
 ls -lh *.xz
+[ "$GITHUB_EVENT_NAME" == "pull_request" ] && exit 0
+
 echo "SF_USER_MAPPED: $SF_USER_MAPPED"
 sshpass -p $SF_PW_MAPPED scp -o StrictHostKeyChecking=no ffmpeg-${SUFFIX}.tar.xz $SF_USER_MAPPED,avbuild@frs.sourceforge.net:/home/frs/project/a/av/avbuild/${TARGET_OS}
