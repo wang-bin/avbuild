2696eda7a78c39e69e457c76b0ddd1d9e1f090de
wangbin <wbsecg1@gmail.com>
2026-07-01 09:29:18 +0800

    9.0 patches

--- avbuild.sh
+++ avbuild.sh
@@ -111,8 +111,9 @@ PATCH_BRANCH=master
 [ $FFMAJOR -eq 5 ] && PATCH_BRANCH=5.1
 [ $FFMAJOR -eq 6 ] && PATCH_BRANCH=6.$FFMINOR
 [ $FFMAJOR -eq 7 ] && PATCH_BRANCH=7.$FFMINOR
-[ $FFMAJOR -eq 8 ] && {
-    $FFGIT || PATCH_BRANCH=8.$FFMINOR
+[ $FFMAJOR -eq 8 ] && PATCH_BRANCH=8.$FFMINOR
+[ $FFMAJOR -eq 9 ] && {
+    $FFGIT || PATCH_BRANCH=9.$FFMINOR
 }
 echo "FFmpeg/Libav version: $FFMAJOR.$FFMINOR  git: $FFGIT. patch set version: $PATCH_BRANCH"
 USE_VK=$FFGIT
